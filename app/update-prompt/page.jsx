58460b2a1e2589b4d70f3ecba4a583942a63eb93
khaldon <alsaidi.khaldon@gmail.com>
2025-02-01 02:31:04 +0100

    fix error useSearchParams in deplying with vercel

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { Suspense } from "react";
 import { useState, useEffect } from "react";
 
 import { useRouter, useSearchParams } from "next/navigation";
@@ -57,6 +57,7 @@ const EditPrompt = () => {
   };
 
   return (
+    <Suspense>
     <Form
       type="Edit"
       post={post}
@@ -64,6 +65,7 @@ const EditPrompt = () => {
       submitting={submitting}
       handleSubmit={updatePrompt}
     />
+    </Suspense>
   );
 };
 
